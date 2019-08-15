3a0a6f41573c57649ac988a15691f3abfce23742
litch0 <litch0s@gmail.com>
2019-08-15 14:31:35 +0900

    database error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import Customer from './components/Customer'
+import CustomerAdd from './components/CustomerAdd';
+
 
 import './App.css';
 
@@ -66,7 +68,7 @@ class App extends Component {
 
         this.timer = setInterval(this.progress, 20);
 
- 
+
 
     }
 
@@ -103,67 +105,68 @@ class App extends Component {
 
 
     render() {
-
-        const { classes } = this.props;
-
+        const { classes } = thins.props;
         return (
+            <div>
 
-            <Paper className={classes.root}>
+                <Paper className={classes.root}>
 
-                <Table className={classes.table}>
+                    <Table className={classes.table}>
 
-                    <TableHead>
-
-                        <TableRow>
-
-                            <TableCell>번호</TableCell>
-
-                            <TableCell>이미지</TableCell>
-
-                            <TableCell>이름</TableCell>
-
-                            <TableCell>생년월일</TableCell>
-
-                            <TableCell>성별</TableCell>
-
-                            <TableCell>직업</TableCell>
-
-                        </TableRow>
-
-                    </TableHead>
-
-                    <TableBody>
-
-                        {this.state.customers ?
-
-                            this.state.customers.map(c => {
-
-                                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-
-                            }) :
+                        <TableHead>
 
                             <TableRow>
 
-                                <TableCell colSpan="6" align="center">
+                                <TableCell>번호</TableCell>
 
-                                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                                <TableCell>이미지</TableCell>
 
-                                </TableCell>
+                                <TableCell>이름</TableCell>
+
+                                <TableCell>생년월일</TableCell>
+
+                                <TableCell>성별</TableCell>
+
+                                <TableCell>직업</TableCell>
 
                             </TableRow>
 
-                        }
+                        </TableHead>
 
-                    </TableBody>
+                        <TableBody>
 
-                </Table>
+                            {this.state.customers ?
 
-            </Paper>
+                                this.state.customers.map(c => {
+
+                                    return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+
+                                }) :
+
+                                <TableRow>
+
+                                    <TableCell colSpan="6" align="center">
+
+                                        <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+
+                                    </TableCell>
+
+                                </TableRow>
+
+                            }
+
+                        </TableBody>
+
+                    </Table>
+
+                </Paper>
+
+                <CustomerAdd />
+
+            </div>
 
         );
-
     }
-
 }
 
 
